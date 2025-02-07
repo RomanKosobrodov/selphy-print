0160f49e83078f1e1483cc4cc1f98401392985d5
Roman <roman@kosobrodov.net>
2025-02-07 13:54:13 +1100

    experimenting with win32 printing

--- selphyprint/main.py
+++ selphyprint/main.py
@@ -1,6 +1,9 @@
 import argparse
 import os
-from PIL import Image, UnidentifiedImageError
+import win32print
+import win32ui
+from PIL import Image, UnidentifiedImageError, ImageWin
+
 
 WIDTH = 150
 HEIGHT = 100
@@ -11,12 +14,14 @@ SPACING = 1
 STEPS = 10
 
 LEFT = 7 * SPACING
-RIGHT = 6 * SPACING
+RIGHT = 7 * SPACING
 TOP = 4 * SPACING
 BOTTOM = 4 * SPACING
 
 WIDTH_PX = int(WIDTH / INCH * DPI)
 HEIGHT_PX = int(HEIGHT / INCH * DPI)
+
+MAX_BORDER = 30
 
 X0 = int(LEFT / INCH * DPI)
 X1 = int(RIGHT / INCH * DPI)
@@ -25,29 +30,87 @@ Y1 = int(BOTTOM / INCH * DPI)
 W = WIDTH_PX - X1 - X0
 H = HEIGHT_PX - Y1 - Y0
 
+PRINTER_NAME = "SELPHY"
+PHYSICAL_WIDTH = 110
+PHYSICAL_HEIGHT = 111
 
-def process_image(input_filename, border_pixels, output_filename):
+def process_image(input_filename, border_pixels):
     background = Image.new(mode="RGB", size=(WIDTH_PX, HEIGHT_PX), color=(255, 255, 255))
     try:
         with Image.open(input_filename, "r") as im:
+            width, height = im.width, im.height
             if im.height > im.width:
-                im = im.rotate(angle=90)
-            scale_x = W / im.width
-            scale_y = H / im.height
+                im = im.rotate(angle=270, expand=True)
+                width, height = height, width
+            scale_x = (W - 2 * border_pixels) / width
+            scale_y = (H - 2 * border_pixels) / height
             scale = min(scale_x, scale_y)
-            w = int(scale * im.width - border_pixels)
-            h = int(scale * im.height - border_pixels)
-            im = im.resize(size=(w, h), resample=Image.Resampling.NEAREST)
+            w = int(scale * width)
+            h = int(scale * height)
             offset_x = int((WIDTH_PX - w) / 2)
             offset_y = int((HEIGHT_PX - h) / 2)
+            im = im.resize(size=(w, h), resample=Image.Resampling.NEAREST)
             background.paste(im, box=(offset_x, offset_y))
-            background.save(output_filename, dpi=(DPI, DPI))
+            return background
     except UnidentifiedImageError:
-        print(f"Unsupported image file \"{args.input}\"")
+        print(f"Unsupported image file \"{input_filename}\"")
+
+
+def get_printer_name():
+    for p in win32print.EnumPrinters(win32print.PRINTER_ENUM_LOCAL):
+        if PRINTER_NAME in p[2].upper():
+            return p[2]
+    return None
+
+
+def print_image(im, print_job_name):
+    printer_name = get_printer_name()
+    if printer_name is None:
+        print("Unable to find Canon SELPHY amongst local printers")
+        exit(4)
+
+    # bmp = im.convert("RGB").tobytes("raw", "BGR")
+    # printer = win32print.OpenPrinter(printer_name)
+
+
+    context = win32ui.CreateDC()
+    context.CreatePrinterDC(printer_name)
+    printer_width = context.GetDeviceCaps(PHYSICAL_WIDTH)
+    printer_height = context.GetDeviceCaps(PHYSICAL_HEIGHT)
+
+    context.StartDoc(print_job_name)
+    context.StartPage()
+
+    if im.width < im.height:
+        im = im.rotate(angle=90, expand=True)
+
+    dib = ImageWin.Dib(im)
+    output_handle = context.GetHandleOutput()
+    dib.draw(output_handle, (0, 0, printer_width, printer_height))
+
+    context.EndPage()
+    context.EndDoc()
+    context.DeleteDC()
+    #
+    #
+    # win32print.StartDocPrinter(printer, 1, (print_job_name, None, "RAW"))
+    # win32print.StartPagePrinter(printer)
+    # win32print.WritePrinter(printer, bmp)
+    # win32print.EndPagePrinter(printer)
+    # win32print.EndDocPrinter(printer)
+    # win32print.ClosePrinter(printer)
+
+
+def output_image(im, output_filename, print_job_name="image"):
+    if len(output_filename) > 0:
+        im.save(output_filename, dpi=(DPI, DPI))
+    else:
+        print_image(im, print_job_name)
 
 
 def main():
-    parser = argparse.ArgumentParser(description="Adjust image to print on Canon Selphy without cropping")
+    parser = argparse.ArgumentParser(
+        description="Adjust image to print on Canon Selphy without cropping, print it or save to file")
     parser.add_argument("--input", "-i",
                         help="Input image or directory with multiple images",
                         type=str,
@@ -58,31 +121,40 @@ def main():
                         default=0.0,
                         required=False)
     parser.add_argument("--output", "-o",
-                        help="Output image or directory",
+                        help="(optional) Output filename or directory for processed images."
+                             "If not provided the image will be sent to the printer",
                         type=str,
-                        required=True)
+                        default="",
+                        required=False)
     args = parser.parse_args()
 
     if not os.path.exists(args.input):
-        print(f"Input path \"{args.input}\" does not exist")
+        print(f"Input path \"{args.input}\" does not exist.")
         exit(1)
 
-    output_parent = os.path.dirname(args.output)
-    if not os.path.isdir(output_parent):
-        print(f"Parent directory \"{output_parent}\" must exist")
-        exit(2)
+    if len(args.output) > 0:
+        output_parent = os.path.dirname(args.output)
+        if not os.path.isdir(output_parent):
+            print(f"Parent directory \"{output_parent}\" must exist.")
+            exit(2)
+
+    if args.border > MAX_BORDER:
+        print(f"Border cannot exceed {MAX_BORDER} mm.")
+        exit(3)
 
     border_px = int(args.border / INCH * DPI)
 
     if os.path.isfile(args.input):
-        process_image(args.input, border_px, args.output)
+        result = process_image(args.input, border_px)
+        output_image(result, args.output, args.input)
     else:
         root, subdirectory, files = next(os.walk(args.input))
         for filename in files:
             fn = os.path.join(root, filename)
             basename, ext = os.path.splitext(filename)
             fn_output = os.path.join(args.output, basename + "-print" + ext)
-            process_image(fn, border_px, fn_output)
+            result = process_image(fn, border_px)
+            output_image(result, fn_output, filename)
 
 
 if __name__ == "__main__":
